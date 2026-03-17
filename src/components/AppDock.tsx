83d39e58eb92ff0e03fc150d54e520ac394fe7f5
Anzic23 <37106885+Anzic23@users.noreply.github.com>
2026-03-17 23:23:33 +0300

    Update AppDock.tsx
    new jellyfin apk

--- src/components/AppDock.tsx
+++ src/components/AppDock.tsx
@@ -84,7 +84,7 @@ const baseApps: Array<Omit<AppItem, 'enabled'>> = [
   { id: 'rutube', name: 'RuTube', package: 'rtb.mobile.android', scheme: '', path: '', fallbackUrl: 'https://rutube.ru', icon: rutubeIcon, showOnLaunch: true },
   { id: 'poweramp', name: 'Poweramp', package: 'com.maxmpz.audioplayer', class: 'com.maxmpz.audioplayer.activity.MusicActivity', scheme: 'poweramp', path: '', fallbackUrl: 'https://play.google.com/store/apps/details?id=com.maxmpz.audioplayer', icon: powerampIcon, showOnLaunch: true },
   { id: 'lampa', name: 'Lampa', package: 'top.rootu.lampa', scheme: '', path: '', fallbackUrl: 'http://lampa.mx/', icon: lampaIcon, showOnLaunch: true },
-  { id: 'jellyfin', name: 'Jellyfin', package: 'org.jellyfin.mobile', class: 'org.jellyfin.mobile.MainActivity', scheme: '', path: '', fallbackUrl: 'https://play.google.com/store/apps/details?id=org.jellyfin.mobile', icon: jellyfinIcon, showOnLaunch: true },
+  { id: 'jellyfin', name: 'Jellyfin', package: 'org.jellyfin.mobile', scheme: 'jellyfin', path: '', fallbackUrl: 'https://play.google.com/store/apps/details?id=org.jellyfin.mobile', icon: jellyfinIcon, showOnLaunch: true },
 ];
 
 const defaultApps: AppItem[] = baseApps.map(app => ({
@@ -146,12 +146,9 @@ export const AppDock = () => {
 
     if (schemeUrl) {
       window.location.href = schemeUrl;
-      if (app.fallbackUrl) {
-        setTimeout(() => window.open(app.fallbackUrl, '_blank'), 700);
-      }
       return;
     }
-
+    
   // Запуск по package name через Android Intent URL (для приложений без URI-схемы)
     if (app.package) {
       const intentUrl = `intent://#Intent;package=${app.package};action=android.intent.action.MAIN;category=android.intent.category.LAUNCHER;end`;
